72b424a660f9fd2a32092ecbe060dccf145cbf47
RomoloMarotta <romolo.marotta@gmail.com>
2022-07-30 12:07:29 +0200

    Published 20/07/2022 results

---
+++ source/esami/build_tab.py
@@ -0,0 +1,42 @@
+f = open("lug.txt")
+
+sep="+---+-------------+------+------+------+------------+------+-------------+----------+"
+h1= "|   |             |  **TEORIA**                     | **PROGRAMMAZIONE** |          |"
+h2= "|   +             +------+------+------+------------+------+-------------+          +"
+h3= "|#  |**MATRICOLA**|**Q1**|**Q2**|**Q3**|**PARZIALE**|**Q4**|**CHALLENGE**|**TOTALE**|"
+
+
+res=[sep,h1,h2,h3,sep]
+for l in res:
+    print(l)
+lens=[13, 6, 6, 6, 12, 6, 13, 10, 10]
+count=1
+for line in f.readlines():
+    spa = 2
+    if count >= 10:
+        spa-=1
+    if count >= 100:
+        spa-=1
+    line=line.strip()
+    if line == "":
+        continue
+    if "Assente" in line:
+        continue
+    while "  " in line:
+        line = line.replace("  ", " ")
+    line = line.replace("Ritirato", "Insuf.")
+    line = line.split(" ")
+
+    res = [" "*spa+str(count)]
+    i=0
+    for l in line:
+        if i == 7:
+            i+=1
+            continue
+        res += [" "*(lens[i]-len(l))+l]
+        i+=1
+    print("|"+"|".join(res)+"|")
+    count+=1
+    print(sep)
+
+    
